62f873af6869321426f975592dd6e1afefd40c99
islomnumanov <islom4ik.numanov@gmail.com>
2021-07-06 23:30:54 +0900

    prototypal inheritance

--- hangman.js
+++ hangman.js
@@ -2,13 +2,29 @@
 // 2. Setup two attributes. One for the word itself. Another for the number of guesses allowed.
 // 3. Create two instances of it and print both to the console
 
-const Hangman = function (word, guesses) {
-    this.word = word
-    this.guesses = guesses
+// 1. Set up the word instance property as an array of lower case letters
+// 2. Set up another instance property to store guessed letters
+// 3. Create a method that gives you the word puzzle back
+
+const Hangman = function (word, guessesRemaining) {
+    this.word = word.toLowerCase().split("")
+    this.guessesRemaining = guessesRemaining
+    this.guessedLetters = ['l','s']
 }
 
-const instance1 = new Hangman('Mercury', 5)
-console.log(instance1);
+Hangman.prototype.getPuzzle = function () {
+    let puzzle = ''
 
-const instance2 = new Hangman('Neptune', 2)
-console.log(instance2);
+    this.word.forEach((letter) => {
+        if (this.guessedLetters.includes(letter) || letter === ' ') {
+            puzzle += letter
+        } else {
+            puzzle += '*'
+        }
+    })
+
+    return puzzle
+}
+
+const me = new Hangman('Alex Sanchez', 22)
+console.log(me.getPuzzle());
--- person.js
+++ person.js
@@ -1,13 +1,35 @@
-const Person = function (firstName, lastName, age) {
+// Prototypal Inheritance
+
+const Person = function (firstName, lastName, age, likes = []) {
     this.firstName = firstName
     this.lastName = lastName
     this.age = age
+    this.likes = likes
+
 
 }
 
-const me = new Person('David', 'Bowie', 78)
-console.log(me.age);
+Person.prototype.getBio = function () {
+    let bio = `${this.firstName} is ${this.age}.`
+
+    this.likes.forEach((like) => {
+        bio += ` ${this.firstName} likes ${like}.`
+    })
+
+
+    return bio
+}
+
+Person.prototype.setName = function (fullName) {
+    const names = fullName.split(' ')
+    this.firstName = names[0]
+    this.lastName = names[1]
+}
+
+const me = new Person('David', 'Bowie', 78, ['Cooking', 'Walking'])
+me.setName('Michael Jackson')
+console.log(me.getBio());
 
 const friend = new Person('Jen', 'Grand', 29)
-console.log(friend);
+console.log(friend.getBio());
 
