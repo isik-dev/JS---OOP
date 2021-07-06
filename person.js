a532304aeb48fd5787e158d4dbeb81c2b8da9b8e
islomnumanov <islom4ik.numanov@gmail.com>
2021-07-06 19:46:30 +0900

    started oop

---
+++ person.js
@@ -0,0 +1,12 @@
+const Person = function (firstName, lastName, age) {
+    this.firstName = firstName
+    this.lastName = lastName
+    this.age = age
+
+}
+
+const me = new Person('David', 'Bowie', 78)
+console.log(me.age);
+
+const friend = new Person('Jen', 'Grand', 29)
+console.log(friend);
