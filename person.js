f848cf2e5ab0256eff25724313e2236489f6e646
islomnumanov <islom4ik.numanov@gmail.com>
2021-07-07 20:30:02 +0900

    done

--- person.js
+++ person.js
@@ -1,35 +1,28 @@
 // Prototypal Inheritance
 
-const Person = function (firstName, lastName, age, likes = []) {
-    this.firstName = firstName
-    this.lastName = lastName
-    this.age = age
-    this.likes = likes
+class Person {
+    constructor(firstName, lastName, age, likes = []) {
+        this.firstName = firstName
+        this.lastName = lastName
+        this.age = age
+        this.likes = likes
+    }
+    getBio() {
+        let bio = `${this.firstName} is ${this.age}.`
 
-
+        this.likes.forEach((like) => {
+            bio += ` ${this.firstName} likes ${like}.`
+        })    
+        return bio 
+    }
+    setName(fullName) {
+        const names = fullName.split(' ')
+        this.firstName = names[0]
+        this.lastName = names[1]
+    }
 }
 
-Person.prototype.getBio = function () {
-    let bio = `${this.firstName} is ${this.age}.`
-
-    this.likes.forEach((like) => {
-        bio += ` ${this.firstName} likes ${like}.`
-    })
-
-
-    return bio
-}
-
-Person.prototype.setName = function (fullName) {
-    const names = fullName.split(' ')
-    this.firstName = names[0]
-    this.lastName = names[1]
-}
-
-const me = new Person('David', 'Bowie', 78, ['Cooking', 'Walking'])
-me.setName('Michael Jackson')
-console.log(me.getBio());
-
-const friend = new Person('Jen', 'Grand', 29)
-console.log(friend.getBio());
+const biggie = new Person('Joseph', 'Novilsky', 26, ['Tech', 'Swimming'])
+biggie.setName('Michael Phelps')
+console.log(biggie.getBio());
 
